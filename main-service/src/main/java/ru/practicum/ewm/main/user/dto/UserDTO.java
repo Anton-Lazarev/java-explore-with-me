79e7ee6c52d3897491ae60876eb223c9ca0150d7
Anton-Lazarev <big.panci@yandex.ru>
2023-07-06 11:18:22 +0300

    feat : add error messages

--- main-service/src/main/java/ru/practicum/ewm/main/user/dto/UserDTO.java
+++ main-service/src/main/java/ru/practicum/ewm/main/user/dto/UserDTO.java
@@ -12,10 +12,10 @@ import javax.validation.constraints.Size;
 public class UserDTO {
     private long id;
     @NotBlank(message = "Name of user can't be blank, empty or null")
-    @Size(min = 2, max = 250)
+    @Size(min = 2, max = 250, message = "Size of name should be between 2 and 250")
     private String name;
     @NotBlank(message = "Email of user can't be blank, empty or null")
-    @Size(min = 6, max = 254)
+    @Size(min = 6, max = 254, message = "Size of email should be between 6 and 254")
     @Email
     private String email;
 }
